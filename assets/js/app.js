106cb6c0c99584deea1adc98567e28babea88856
hu0599 <7ussein.5alil@gmail.com>
2020-03-02 17:23:26 -0500

    comands added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 /* JS Main Scripts*/
+$(document).ready(function(){
+  $("a").on('click', function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
